4a2f8dcce402d677f4d20c608386441eb559bc1b
Adriano Massi <adrianomassi@gmail.com>
2022-12-01 17:28:23 +0100

    End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
-
+import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CardFormComponent
+    CardFormComponent,
+    InputComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { DateFormControl } from '../date-form-control';
 @Component({
   selector: 'app-card-form',
   templateUrl: './card-form.component.html',
@@ -10,8 +10,23 @@ export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(3),
+      Validators.maxLength(5)
     ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    expiration: new DateFormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3)
+    ])
   });
 
   constructor() { 
@@ -19,6 +34,14 @@ export class CardFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log('Form was submitted');
+  }
+
+  onResetClick() {
+    this.cardForm.reset();
   }
 
 }
